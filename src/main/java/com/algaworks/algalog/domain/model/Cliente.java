478c5abacd541cba456bd1795d298ffee992e2c1
Lucas Gomes Matos <lgomes.lg99@gmail.com>
2022-09-26 07:42:08 -0300

    Validate Jacarta EE

--- src/main/java/com/algaworks/algalog/domain/model/Cliente.java
+++ src/main/java/com/algaworks/algalog/domain/model/Cliente.java
@@ -6,6 +6,9 @@ import lombok.Getter;
 import lombok.Setter;
 
 import javax.persistence.*;
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
 
 
 @EqualsAndHashCode(onlyExplicitlyIncluded = true)
@@ -19,9 +22,17 @@ public class Cliente {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long Id;
 
+    @NotBlank
+    @Size(max = 60)
     private String nome;
+
+    @NotBlank
+    @Email
+    @Size(max = 255)
     private String email;
 
+    @NotBlank
+    @Size(max = 20)
     @Column(name = "fone")
     private String telefone;
 
